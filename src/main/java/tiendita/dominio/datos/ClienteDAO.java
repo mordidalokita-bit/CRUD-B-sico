8012bf6379fef7dc4ec9d42b608e7848bd08525e
mordidalokita-bit <mordidalokita@gmail.com>
2026-06-15 17:15:17 -0500

    Creando Prueba de ClienteDAO a Base de Datos

--- src/main/java/tiendita/dominio/datos/ClienteDAO.java
+++ src/main/java/tiendita/dominio/datos/ClienteDAO.java
@@ -2,9 +2,15 @@ package tiendita.dominio.datos;
 
 import tiendita.dominio.Modelo.Cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ClienteDAO implements IClienteDAO{
+import static tiendita.dominio.conexion.Conexion.getConexion; //Metodo Estatico
+
+public class ClienteDAO implements IClienteDAO {
 
     // JALAMOS LOS METODOS CRUD DE LA INTERFACE IClienteDAO
 
@@ -12,7 +18,37 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public List<Cliente> listarClientes() {
-        return List.of();
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        var sql = "SELECT * FROM cliente ORDER BY id";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setDescuento(rs.getInt("descuento"));
+                clientes.add(cliente);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al listar Clientes: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+
+
+
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexión ");
+            }
+
+        }
+        return clientes;
     }
 
     @Override
@@ -35,7 +71,14 @@ public class ClienteDAO implements IClienteDAO{
         return false;
     }
 
+    static void main() {
+        // LISTAR CLIENTES
+        System.out.println("*** LISTAR CLIENTES ***");
+
+        IClienteDAO clienteDao = new ClienteDAO();
+        var clientes = clienteDao.listarClientes();
+        clientes.forEach(System.out::println);
 
 
-
- }
+    }
+}
